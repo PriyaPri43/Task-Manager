2c21110c9067f3c5046226d75a895e0c26a5eff6
PriyaPri43 <pridiv49@gmail.com>
2026-02-07 07:28:03 +0530

    backend url updated

--- Frontend/task-manager/src/api.jsx
+++ Frontend/task-manager/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api"
+  baseURL: "https://task-manager-2-v9rl.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
